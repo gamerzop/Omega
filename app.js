15f152158c51698d8e3d03614c6474e618244e23
gamerzop <70996655+gamerzop@users.noreply.github.com>
2020-09-09 10:35:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,15 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyCt0J--qUxbjWpFop4UphwJH_wnBpXFaGk",
+  authDomain: "game-9cbfc.firebaseapp.com",
+  databaseURL: "https://game-9cbfc.firebaseio.com",
+  projectId: "game-9cbfc",
+  storageBucket: "game-9cbfc.appspot.com",
+  messagingSenderId: "932784001827",
+  appId: "1:932784001827:web:2be4650c921bb26b71f296",
+  measurementId: "G-0W5P4KMZQY"
 };
 
      
